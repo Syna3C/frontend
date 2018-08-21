0a2fcd23580dd9e70fff441634bd77968a112d88
Vinay Gopinath <vinayg18@gmail.com>
2018-08-21 21:12:26 +0530

    [Deploy] Azure continuous deployment script

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,7 +1,7 @@
 export class ApiConstants {
-  public static BASE_URL = process.env.REACT_APP_BASE_URL;
+  public static BASE_URL = process.env.REACT_APP_CUSTOM_BASE_URL || process.env.REACT_APP_BASE_URL;
   public static API_VERSION = 1;
-  public static API_URL = `${process.env.REACT_APP_API_URL}/v${ApiConstants.API_VERSION}`;
+  public static API_URL = `${process.env.REACT_APP_CUSTOM_API_URL || process.env.REACT_APP_API_URL}/v${ApiConstants.API_VERSION}`;
   // Users
   public static LOGIN_URL = `${ApiConstants.API_URL}/login`;
   public static SIGNUP_URL = `${ApiConstants.API_URL}/signUp`;
